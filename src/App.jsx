27a8e59f41e82ce9e8fc88814c0f6628ff2c2db3
arunanj2005 <arunanj2005@gmail.com>
2025-11-11 15:07:50 +0530

    Updated arunan J App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div>
       <h1>Hi, CI/CD!</h1>
-      <p>My React app is running from Jenkins!</p>
+      <p>My React app is running from Jenkins! Running made possible by ARUNAN J - 22011102008 </p>
     </div>
   )
 }
