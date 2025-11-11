40793bdcac2bb34dded8b4af2ad7bc8edae3eb60
arunanj2005 <arunanj2005@gmail.com>
2025-11-11 15:18:25 +0530

    Updated with mam's name App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div>
       <h1>Hi, CI/CD!</h1>
-      <p>My React app is running from Jenkins! Running made possible by ARUNAN J - 22011102008 </p>
+      <p>My React app is running from Jenkins! Running made possible by Dr.DHIVYA SUBBURAM , My only favourite staff in the whole university . </p>
     </div>
   )
 }
